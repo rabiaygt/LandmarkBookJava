d379b5d6d8908fa9d5f59b429ec8fdfb9c774566
Rabia Yiğit <rabia.ygt15@gmail.com>
2024-08-12 20:01:45 +0300

    LandmarkBookJava

---
+++ app/src/main/java/com/rabiayigit/landmarkbookjava/DetailsActivity.java
@@ -0,0 +1,36 @@
+package com.rabiayigit.landmarkbookjava;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.rabiayigit.landmarkbookjava.databinding.ActivityDetailsBinding;
+
+public class DetailsActivity extends AppCompatActivity {
+    private ActivityDetailsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDetailsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        //Landmark selectedLandmark = (Landmark) intent.getSerializableExtra("landmark");
+
+        Singleton singleton = Singleton.getInstance();
+        Landmark selectedLandmark = singleton.getSelectedLandmark();
+
+        binding.landmarkNameText.setText(selectedLandmark.name);
+        binding.countryNameText.setText(selectedLandmark.country);
+        binding.imageView.setImageResource(selectedLandmark.image);
+
+
+    }
+
+}
